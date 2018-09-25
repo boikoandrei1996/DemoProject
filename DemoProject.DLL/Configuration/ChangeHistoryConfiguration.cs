069c7fd42d89a857883308185363cc44c954d54b
Boiko Andrei <BoikoAndrei1996@gmail.com>
2018-09-25 09:25:37 +0300

    ChangeHistory feature forgotten files

---
+++ DemoProject.DLL/Configuration/ChangeHistoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DemoProject.DLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DemoProject.DLL.Configuration
+{
+  class ChangeHistoryConfiguration : IEntityTypeConfiguration<ChangeHistory>
+  {
+    public void Configure(EntityTypeBuilder<ChangeHistory> builder)
+    {
+      builder
+        .Property(x => x.TableName)
+        .IsRequired();
+
+      builder
+        .Property(x => x.TimeOfChange)
+        .IsRequired();
+    }
+  }
+}
---
+++ DemoProject.DLL/Models/ChangeHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DemoProject.DLL.Models
+{
+  public class ChangeHistory : BaseEntity
+  {
+    public string TableName { get; set; }
+    public DateTime TimeOfChange { get; set; }
+
+    public static ChangeHistory Create(string tableName)
+    {
+      return new ChangeHistory
+      {
+        TableName = tableName,
+        TimeOfChange = DateTime.UtcNow
+      };
+    }
+  }
+
+  public static class TableNames
+  {
+    public static readonly string Discounts = "Discounts";
+    public static readonly string InfoObjects = "InfoObjects";
+  }
+}
