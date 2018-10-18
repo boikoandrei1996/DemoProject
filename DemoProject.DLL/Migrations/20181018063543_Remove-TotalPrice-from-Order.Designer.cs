42562883fd69d16b871e996aeadfc3424d64ba01
Boiko Andrei <BoikoAndrei1996@gmail.com>
2018-10-18 09:37:05 +0300

    remove TotalPrice from order

--- DemoProject.DLL/Migrations/20181018063543_Remove-TotalPrice-from-Order.Designer.cs
+++ DemoProject.DLL/Migrations/20181018063543_Remove-TotalPrice-from-Order.Designer.cs
@@ -4,14 +4,16 @@ using DemoProject.DLL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DemoProject.DLL.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20181018063543_Remove-TotalPrice-from-Order")]
+    partial class RemoveTotalPricefromOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DemoProject.DLL/Models/Order.cs
+++ DemoProject.DLL/Models/Order.cs
@@ -1,28 +1,17 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace DemoProject.DLL.Models
 {
   public class Order : BaseEntity
   {
     public string Name { get; set; }
-
     public string Mobile { get; set; }
-
     public string Address { get; set; }
-
     public string Comment { get; set; }
-
     public DateTime DateOfCreation { get; set; }
-
     public DateTime? DateOfApproved { get; set; }
-
     public DateTime? DateOfRejected { get; set; }
-
     public DateTime? DateOfClosed { get; set; }
-
-    [DataType(DataType.Currency)]
-    public decimal TotalPrice { get; set; }
 
     public Guid CartId { get; set; }
     public Cart Cart { get; set; }
