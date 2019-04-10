be9faa2b6fa75607025d59e7d898d5bcfbb0973c
BoikoAndrei1996 <boikoandrei1996@gmail.com>
2019-04-10 23:54:45 +0300

    small code refactor

--- DemoProject.Web/src/components/_shared/authRoute.jsx
+++ DemoProject.Web/src/components/_shared/authRoute.jsx
@@ -18,7 +18,9 @@ export const AuthRoute = ({ component: Component, roles, ...rest }) => (
     }
     else {
       // not logged in so redirect to login page with the return url
+      // <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
       return (<Redirect exact to={RoutePath.LOGIN} />);
     }
-  }} />
-)
+  }}
+  />
+);
